7566169cebf2ff8cc87c546cdc02ac8e14cba6dc
Lynx96 <39319226+Lynx96@users.noreply.github.com>
2024-02-09 16:27:42 -0300

    Atualizar o Tracking.js

--- Tracking.js
+++ Tracking.js
@@ -1,14 +1,14 @@
 const mqtt = require('mqtt');
 const Web3 = require('web3');
 const cron = require('node-cron');
-const contractABI = require('./artifacts/build-info/contracts/AnimalTracking.sol/AnimalTracking.json'); // Replace with your ABI file
+const contractABI = require('./artifacts/build-info/contracts/AnimalTracking.sol/AnimalTracking.json'); // Substituir pelo abi file correto
  
 const mqttBrokerUrl = 'mqtt://test.mosquitto.org';
 const mqttTopic = 'data/tracking';
 
-const web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/45zltduN9c6fEVfiSVeUp0v8SXsm7K52'); // Replace with your Alchemy API key
+const web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/45zltduN9c6fEVfiSVeUp0v8SXsm7K52'); // substituir pelo Alchemy API key
 
-const contractAddress = '0x7B4Cae5555853c323b9842EcB04825f1CfF0375e'; // Replace with your contract address
+const contractAddress = '0x7B4Cae5555853c323b9842EcB04825f1CfF0375e'; // Endereço do contrato
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 const mqttClient = mqtt.connect(mqttBrokerUrl);
@@ -19,7 +19,7 @@ mqttClient.on('connect', () => {
 });
 
 function scheduleDataCapture() {
-    // Schedule the data capture job to run every 3 hours
+    // controla os dados para serem gerados de 3 em 3 horas
     cron.schedule('0 */3 * * *', () => {
         console.log('Fetching and processing tracking data...');
         fetchDataAndProcess();
@@ -27,21 +27,21 @@ function scheduleDataCapture() {
 }
 
 function fetchDataAndProcess() {
-    // Implement the logic to fetch tracking data from MQTT and process it
+    // Implementa a logica e consolida os dados de rastreio do protocolo mqtt para processa-lo
     mqttClient.subscribe(mqttTopic);
 }
 
 mqttClient.on('message', (topic, message) => {
-    // Assuming the message is in JSON format
+    // recebe o dado e transforma para JSON
     const trackingData = JSON.parse(message);
 
-    // Invoke the captureData function in the smart contract
+    // chama a função de captura no smart contract
     contract.methods.storeCaptureData(
         trackingData.animalId,
         trackingData.latitude,
         trackingData.longitude
     ).send({
-        from: '0x96d6b6e1Eccae2D96A331eA7017F3b33Ed5DDB53', // Replace with your sender address
+        from: '0x96d6b6e1Eccae2D96A331eA7017F3b33Ed5DDB53', // endereço de envio 
         gas: 200000 // Set an appropriate gas limit
     }).then((receipt) => {
         console.log('Transaction receipt:', receipt);
